b55e029bbc23f93bdc3aef495c6f0a861a14cecb
halkis0 <e263738@metu.edu.tr>
2025-12-25 23:27:53 +0300

    ChessNotation import fixed

--- frontend/src/pages/PostDetail.js
+++ frontend/src/pages/PostDetail.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { postAPI } from '../services/api';
 import { authService } from '../services/auth';
-import ChessNotation from '../components/ChessNotation';
+import ChessNotation from '../pages/ChessNotation';
 
 const PostDetail = () => {
     const { id } = useParams();
